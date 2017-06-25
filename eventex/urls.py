f55a0fa984d6fb3cbec40efff10a54273ee6f26c
Tiago Amaral <tiago.amaral87@gmail.com>
2017-06-24 22:19:24 -0300

    tela de inscricao

--- eventex/urls.py
+++ eventex/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from eventex.subscriptions.views import subscribe
 
 from eventex.core.views import home
 
 urlpatterns = [
     url(r'^$', home),
+    url(r'^inscricao/$', subscribe),
     url(r'^admin/', include(admin.site.urls)),
 ]
