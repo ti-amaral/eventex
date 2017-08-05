5584f01b7899b88fc0c4264d63153e48d56096db
Tiago Amaral <tiago.amaral87@gmail.com>
2017-08-04 23:34:43 -0300

    admin magic

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100,verbose_name="nome")
+    cpf = models.CharField(max_length=11,verbose_name="CPF")
+    email = models.EmailField(verbose_name="e-mail")
+    phone = models.CharField(max_length=20,verbose_name="telefone")
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name="criado em")
+
+    class Meta:
+        verbose_name_plural = "inscrições"
+        verbose_name = "inscrição"
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return self.name
