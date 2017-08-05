5b78637c3203f8d9e1ac30744ecd18cc51e4f8a8
Tiago Amaral <tiago.amaral87@gmail.com>
2017-08-04 22:47:21 -0300

    persiste a inscricao

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name="Tiago Amaral",
+            cpf="123456789012",
+            email="tiagoamaral87@gmail.com",
+            phone="61-999994450"
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        ''' Subscription must have an auto created_at attr '''
+        self.assertIsInstance(self.obj.created_at, datetime)
