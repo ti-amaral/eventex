1527cf94eaa14bfe8f94ec29feb4284ca4fe5c95
Tiago Amaral <tiago.amaral87@gmail.com>
2017-09-10 21:16:57 -0300

    padroniza nomes

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -20,6 +20,12 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form,'cpf','length')
 
+    def test_name_must_be_capitalized(self):
+        '''Name must be capitalized'''
+        # TIAGO amaral -> Tiago Amaral
+        form = self.make_validated_form(name="TIAGO amaral")
+        self.assertEqual("Tiago Amaral", form.cleaned_data['name'])
+
     def assertFormErrorCode(self,form,field,code):
         errors = form.errors.as_data()
         errors_list = errors[field]
